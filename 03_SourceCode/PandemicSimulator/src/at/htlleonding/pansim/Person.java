76613438ccdd4c21760c839b7cfd0098633ae0db
Jonas Lorenz <jonaslorenz.jl@hotmail.com>
2022-02-18 11:57:51 +0100

    Infrectionsstate update

--- 03_SourceCode/PandemicSimulator/src/at/htlleonding/pansim/Person.java
+++ 03_SourceCode/PandemicSimulator/src/at/htlleonding/pansim/Person.java
@@ -1,15 +1,12 @@
 package at.htlleonding.pansim;
 
-public class Person {
+public class Person implements Updatable{
     private InfectionState infectionState;
     private double transmitability;
     private double immunitivity;
     private double probOfDeath;
-    private boolean qurantined;
-
-    public Person(){
-
-    }
+    private boolean quarantined;
+    private int timer;
 
     private void participateInEvent(Event event){
 
@@ -20,11 +17,12 @@ public class Person {
         this.transmitability = 0;
         this.immunitivity = 0;
         // this.probOfDeath = probOfDeath;
-        this.qurantined = false;
+        this.quarantined = false;
     }
 
     private void infect(){
         this.infectionState = InfectionState.INFECTED;
+        timer = 10;
     }
 
     public void vaccinate(){
@@ -32,10 +30,26 @@ public class Person {
     }
 
     private void recover(){
-
+        this.infectionState = InfectionState.RECOVERED;
     }
 
-    public void update(){
+    @Override
+    public void update(int generation) {
+        switch (infectionState){
+            case INFECTED:
+                timer--;
+                if (timer <= 0){
+                    recover();
+                    timer = 180;
+                }
+                break;
+            case RECOVERED:
+                timer--;
+                if (timer <= 0){
+                    infectionState = InfectionState.SUSCEPTABLE;
+                }
+                break;
 
+        }
     }
 }

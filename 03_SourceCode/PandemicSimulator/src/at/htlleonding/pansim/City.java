6911206f693e6760c56d017901321031981da3c9
OrtixD <domi.ortbauer@gmail.com>
2022-03-04 08:26:12 +0100

    fixed typo + added City Test

--- 03_SourceCode/PandemicSimulator/src/at/htlleonding/pansim/City.java
+++ 03_SourceCode/PandemicSimulator/src/at/htlleonding/pansim/City.java
@@ -10,15 +10,35 @@ public class City {
     private Virus virus;
     private Renderer renderer;
     private int generation;
+    private int endGeneration = 365;
+
+    public City(int amountOfPeople, int endGeneration) {
+        generation = 0;
+        virus = new Virus();
+        this.endGeneration = endGeneration;
+
+        initPeople(amountOfPeople);
+    }
 
     private void update(){
+        for(generation = 0; generation < endGeneration; generation++) {
+            for(Person p : people) {
+                p.update(generation);
+            }
 
+            for(Event e : events) {
+                e.update(generation);
+            }
+        }
     }
 
     public void startSimulation(){
-        generation = 0;
-        virus = new Virus();
-
         update();
+    }
+
+    private void initPeople(int amountOfPeople) {
+        for (int i = 0; i < amountOfPeople; i++) {
+            people.add(new Person());
+        }
     }
 }

65a7130955f8b52881f50b2b2f3c0fd251c6fede
OrtixD <domi.ortbauer@gmail.com>
2022-03-04 11:07:19 +0100

    changed Event + EventType

---
+++ 03_SourceCode/PandemicSimulator/test/at/htlleonding/pansim/EventUT.java
@@ -0,0 +1,18 @@
+package at.htlleonding.pansim;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EventUT {
+
+    @Test
+    void testJoinShallReturnTrueGivenAPerson() {
+        Event event = new Event(EventType.TEST);
+    }
+
+    @Test
+    void leave() {
+    }
+}
